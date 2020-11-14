0ce8857f1f22cde42dc1869c5e3a8870d5f35b08
AdirYed <adir.yed@gmail.com>
2020-11-14 14:41:21 +0200

    added columns to user

--- database/migrations/2020_11_14_192243_create_users_table.php
+++ database/migrations/2020_11_14_192243_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,12 +16,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('points')->default(0);
+            $table->unsignedBigInteger('total_points_earned')->default(0);
+            $table->string('role')->default(User::ROLE_USER)->index();
+            $table->string('ip')->nullable();
+            $table->unsignedBigInteger('referred_by')->nullable();
+            $table->timestamp('banned_at')->nullable();
+            $table->string('ban_reason')->nullable();
             $table->timestamps();
+
+            $table->foreign('referred_by')->references('id')->on('users')->onDelete('set null');
         });
     }
 
