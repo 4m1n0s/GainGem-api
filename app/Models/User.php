86ff22b9e9e33ad997bad33a3a7b4fe58be0f7bf
AdirYed <adir.yed@gmail.com>
2020-11-14 17:27:32 +0200

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\DB;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
