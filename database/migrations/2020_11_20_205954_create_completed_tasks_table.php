5d7a30e096493d39a7e0a0e230a27e6db3cffe90
AdirYed <adir.yed@gmail.com>
2020-11-20 23:28:02 +0200

    added tables

---
+++ database/migrations/2020_11_20_205954_create_completed_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompletedTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('completed_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('provider')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('points');
+            $table->json('data')->nullable();
+            $table->unsignedBigInteger('coupon_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('coupon_id')->references('id')->on('coupons')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('completed_tasks');
+    }
+}
